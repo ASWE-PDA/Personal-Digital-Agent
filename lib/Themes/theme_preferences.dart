2dc04a024951d0534fdcfddce983354c21f3a03f
Victoria Strobel <victoria.strobel@porsche.de>
2023-03-13 10:04:39 +0100

    add flutter themes and dark mode

---
+++ lib/Themes/theme_preferences.dart
@@ -0,0 +1,24 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// Class that is responsible for loading and updating the theme preference.
+class ThemePreferences {
+  static const themeKey = 'theme_key';
+
+  /// Updates the [value] of the theme.
+  setTheme(bool value) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setBool(themeKey, value);
+  }
+
+  /// Deletes the theme preference.
+  deleteTheme() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.remove(themeKey);
+  }
+
+  /// Loads the value of the stored theme.
+  getTheme() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    return sharedPreferences.getBool(themeKey);
+  }
+}

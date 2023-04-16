3836af7ad004253d5b6bca8a115dee76ad87bf44
Nova <inf20031@lehre.dhbw-stuttgart.de>
2023-04-16 13:29:06 +0200

    Calendar ios implementation

--- lib/Services/Calendar/calendar_service.dart
+++ lib/Services/Calendar/calendar_service.dart
@@ -3,14 +3,19 @@ import 'package:device_calendar/device_calendar.dart';
 Future<List<Event>> getUpcomingEvents() async {
   final calendarPlugin = DeviceCalendarPlugin();
 
-  var permissionsGranted = await calendarPlugin.hasPermissions();
-  if (permissionsGranted.data != null) {
+  try {
+    var permissionsGranted = await calendarPlugin.hasPermissions();
+    if (permissionsGranted.data != null) {
 
-    if (permissionsGranted.isSuccess && !permissionsGranted.data!) {
-      permissionsGranted = await calendarPlugin.requestPermissions();
-      if (!permissionsGranted.isSuccess || !permissionsGranted.data!) return [];
+      if (permissionsGranted.isSuccess && !permissionsGranted.data!) {
+        permissionsGranted = await calendarPlugin.requestPermissions();
+        if (!permissionsGranted.isSuccess || !permissionsGranted.data!) return [];
+      }
     }
   }
+  catch (e) { print("error in permission req ");}
+  
+  
   
   final calendarListResult = await calendarPlugin.retrieveCalendars();
   final calendarId = calendarListResult.data?.isNotEmpty ?? false ? calendarListResult.data?.first.id : null;
@@ -18,7 +23,7 @@ Future<List<Event>> getUpcomingEvents() async {
   if (calendarId != null) {
     final now = DateTime.now();
     final start = DateTime(now.year, now.month, now.day).toUtc();
-    final end = DateTime(now.year, now.month, now.day + 30).toUtc();
+    final end = DateTime(now.year, now.month, now.day + 3).toUtc();
 
     final eventsResult = await calendarPlugin.retrieveEvents(
         calendarId, RetrieveEventsParams(startDate: start, endDate: end));

257a7ecea94f149d5a7e369204fc61cabac228fc
Nova <inf20031@lehre.dhbw-stuttgart.de>
2023-04-17 10:39:30 +0200

    Added Calendar Service

---
+++ lib/UseCases/Scheduling/scheduling_model.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:luna/UseCases/good_night_preferences.dart';
+
+/// Class that stores an the philips hue bridge configuration and notifies the app.
+class SchedulingModel extends ChangeNotifier {
+  GoodNightPreferences goodNightPreferences = GoodNightPreferences();
+  int? _hours;
+  int? _minutes;
+
+  int? get hours => _hours;
+  int? get minutes => _minutes;
+
+  /// Constructor of the [BridgeModel] class.
+  SchedulingModel() {
+    getGoodNightPreferences();
+  }
+
+  String getPreferencesAsString() {
+    if (_hours != null && _minutes != null) {
+      if (_hours! < 10 && _minutes! < 10) {
+        return "0$_hours:0$_minutes";
+      } else if (_hours! < 10) {
+        return "0$_hours:$_minutes";
+      } else if (_minutes! < 10) {
+        return "$_hours:0$_minutes";
+      } else {
+        return "$_hours:$_minutes";
+      }
+    }
+    return "";
+  }
+
+  getGoodNightPreferences() async {
+    _hours = await goodNightPreferences.getHours();
+    _minutes = await goodNightPreferences.getMinutes();
+    notifyListeners();
+  }
+
+  deleteGoodNightPreferences() async {
+    await goodNightPreferences.deleteTime();
+    _hours = null;
+    _minutes = null;
+    notifyListeners();
+  }
+
+  /// Updates the [value] of the stored user name.
+  set setHours(int value) {
+    _hours = value;
+    goodNightPreferences.setHours(value);
+    notifyListeners();
+  }
+
+  /// Updates the [value] of the stored ip address.
+  set setMinutes(int value) {
+    _minutes = value;
+    goodNightPreferences.setMinutes(value);
+    notifyListeners();
+  }
+}
---
+++ lib/UseCases/Scheduling/scheduling_preferences.dart
@@ -0,0 +1,38 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// Class that is responsible for loading and updating the shared preferences for the bridge configuration.
+class SchedulingPreferences {
+  static const hoursKey = 'hours_key';
+  static const minutesKey = 'minutes_key';
+
+  /// Updates the [hours] and [minutes] of the good night schedule time.
+  Future<void> setHours(int hours) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setInt(hoursKey, hours);
+  }
+
+  /// Updates the [hours] and [minutes] of the good night schedule time.
+  Future<void> setMinutes(int minutes) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setInt(minutesKey, minutes);
+  }
+
+  /// Deletes the time stamp.
+  deleteTime() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.remove(hoursKey);
+    sharedPreferences.remove(minutesKey);
+  }
+
+  /// Loads the hours value.
+  getHours() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    return sharedPreferences.getInt(hoursKey);
+  }
+
+  /// Loads the minutes value.
+  getMinutes() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    return sharedPreferences.getInt(minutesKey);
+  }
+}

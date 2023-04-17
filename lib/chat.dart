19c5929fbc49f10792d15fd7405c9eed77f3b539
Nova <inf20031@lehre.dhbw-stuttgart.de>
2023-04-18 00:23:14 +0200

    added movie service

--- lib/chat.dart
+++ lib/chat.dart
@@ -4,7 +4,7 @@ import 'package:flutter_tts/flutter_tts.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:luna/Screens/newsScreen.dart';
 import 'package:luna/Services/Alarm/alarm_service.dart';
-import 'package:luna/Services/Calendar/calendar_service.dart';
+import 'package:luna/Services/Movies/movie_service.dart';
 import 'package:luna/Services/location_service.dart';
 import 'package:luna/Services/maps_service.dart';
 import 'package:luna/Services/SmartHome/smart_home_service.dart';
@@ -185,6 +185,11 @@ class _ChatPageState extends State<ChatPage> {
                     pushNewsScreen();
                   },
                   child: Text("Test News Use Case DEBUG")),
+              ElevatedButton(
+                  onPressed: () async {
+                    getPopularMovies();
+                  },
+                  child: Text("Test Move Service DEBUG")),
               Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: AvatarGlow(

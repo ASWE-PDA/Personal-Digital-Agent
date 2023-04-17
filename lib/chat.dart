a63574b4d1007c6016dbb13ff74d6ac591ab7993
Daimon-M <daimon3580@gmail.com>
2023-04-17 22:34:45 +0200

    update my progress into merge branch
    this is the news use case
    
    missing:
    move news_api into services folder
    create Schedule screen
    create schedule task
    Navigator.push()
    fix display error of newsScreen

--- lib/chat.dart
+++ lib/chat.dart
@@ -11,6 +11,7 @@ import 'package:luna/Services/SmartHome/bridge_model.dart';
 import 'package:luna/Services/notification_service.dart';
 import 'package:luna/UseCases/Scheduling/scheduling_use_case.dart';
 import 'package:luna/UseCases/good_night_use_case.dart';
+import 'package:luna/UseCases/news/news_use_case.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
@@ -170,6 +171,11 @@ class _ChatPageState extends State<ChatPage> {
                     
                   },
                   child: Text("Test Calendar Usecase DEBUG")),
+              ElevatedButton(
+                  onPressed: () async {
+                    NewsUseCase.instance.execute("news");
+                  },
+                  child: Text("Test News Use Case DEBUG")),
               Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: AvatarGlow(

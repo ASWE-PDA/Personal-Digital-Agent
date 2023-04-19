36dd5c3e02f11eb5c52fae109785babfe8e94dff
Nova <c.privat1@web.de>
2023-04-19 23:38:05 +0200

    Event planning time parser

--- test/event_planning_use_case_test.dart
+++ test/event_planning_use_case_test.dart
@@ -1,28 +1,48 @@
-import 'dart:math';
-
-import 'package:flutter/material.dart';
-import 'package:luna/Services/Alarm/alarm_service.dart';
-import 'package:luna/Services/SmartHome/smart_home_model.dart';
-import 'package:luna/Services/SmartHome/smart_home_service.dart';
-import 'package:luna/Services/spotify_service.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:luna/UseCases/EventPlanning/event_planning_use_case.dart';
-import 'package:luna/UseCases/good_night_use_case.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:spotify_sdk/spotify_sdk.dart';
-import 'good_night_use_case_test.mocks.dart';
+import 'package:luna/Services/Calendar/calendar_service.dart';
+import 'package:luna/Services/Movies/movie_service.dart';
 
+import 'event_planning_use_case_test.mocks.dart';
+
+@GenerateMocks([CalendarService, MovieService])
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences.setMockInitialValues({});
+  group('EventPlanningUseCase', () {
+    test('getAllTriggerWords should return all trigger words', () {
+      final eventPlanningUseCase = EventPlanningUseCase.instance;
+      final allTriggerWords = eventPlanningUseCase.getAllTriggerWords();
 
-  test("parser pm", () {
-    var datetime = EventPlanningUseCase.instance.parseSpokenTime("8:00 pm");
-    expect(
-        datetime,
-        DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day,
-            20, 0));
+      expect(allTriggerWords, [
+        ...eventPlanningUseCase.schedulingTriggerWords,
+        ...eventPlanningUseCase.calendarTriggerWords,
+        ...eventPlanningUseCase.movieTriggerWords,
+      ]);
+    });
+
+    test('getTimeFromHoursMinutes should return the correct time', () {
+      final eventPlanningUseCase = EventPlanningUseCase.instance;
+      expect(eventPlanningUseCase.getTimeFromHoursMinutes(1, 0), '1 a.m');
+      expect(eventPlanningUseCase.getTimeFromHoursMinutes(0, 0), '0 a.m');
+      expect(eventPlanningUseCase.getTimeFromHoursMinutes(12, 0), '12 p.m');
+      expect(eventPlanningUseCase.getTimeFromHoursMinutes(13, 0), '1 p.m');
+      expect(eventPlanningUseCase.getTimeFromHoursMinutes(23, 0), '11 p.m');
+      expect(eventPlanningUseCase.getTimeFromHoursMinutes(1, 30), '1:30 a.m');
+      expect(eventPlanningUseCase.getTimeFromHoursMinutes(23, 30), '11:30 p.m');
+    });
+
+    test('parseSpokenTime should return the correct DateTime', () {
+      final eventPlanningUseCase = EventPlanningUseCase.instance;
+      expect(eventPlanningUseCase.parseSpokenTime('1 am')!.hour, 1);
+      expect(eventPlanningUseCase.parseSpokenTime('12 am')!.hour, 0);
+      expect(eventPlanningUseCase.parseSpokenTime('12 pm')!.hour, 12);
+      expect(eventPlanningUseCase.parseSpokenTime('1 pm')!.hour, 13);
+      expect(eventPlanningUseCase.parseSpokenTime('11 pm')!.hour, 23);
+      expect(eventPlanningUseCase.parseSpokenTime('1:30 am')!.minute, 30);
+      expect(eventPlanningUseCase.parseSpokenTime('11:30 pm')!.minute, 30);
+      expect(eventPlanningUseCase.parseSpokenTime('invalid time'), null);
+    });
   });
 }
